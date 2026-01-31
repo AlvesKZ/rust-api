afe868f9787cc5134f0a8afc6b69e4cdbddf4311
AlvesKZ <igoralvex01@gmail.com>
2026-01-31 13:47:27 -0300

    feat: create http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod services;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server starting on http://127.0.0.1:8080");
+
+    HttpServer::new(move || App::new().configure(services::config))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
